7176306cc128800062caf318338f3d504b4b2fcb
Yousef <yuossefbakier@gmail.com>
2026-09-08 21:50:52 +0300

    release: v1.0

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@
  */
 import { useAuthStore } from '../stores/authStore';
 
-const API_BASE = '';
+const API_BASE = (import.meta.env.VITE_API_URL || '').replace(/\/+$/, '');
 
 let isRefreshing = false;
 let refreshSubscribers = [];
